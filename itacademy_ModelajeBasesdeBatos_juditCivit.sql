db0935ac93ce544f9602ce049b5ff32b4edc1438
Judit <jujo351@hotmail.com>
2019-01-08 09:27:49 +0100

    Update itacademy_ModelajeBasesdeBatos_juditCivit.sql
    Revision 1

--- itacademy_ModelajeBasesdeBatos_juditCivit.sql
+++ itacademy_ModelajeBasesdeBatos_juditCivit.sql
@@ -1,4 +1,4 @@
-/*USFLIGHTS EXERCICE*/
+USE usairlineflights;
 
 /* 1.Quantitat de registres de la taula de vols: */
 SELECT count(*) FROM usairlineflights.flight;
@@ -16,35 +16,22 @@ ORDER BY Origin ASC;
 
 /* 4.Retard promig d’arribada dels vols, per mesos i segons l’aeroport origen (mateixa consulta que abans i amb el mateix ordre). 
 Però a més, ara volen que en comptes del codi de l’aeroport es mostri el nom de la ciutat. */
-USE usairlineflights;
 SELECT b.City, Year, Month, AVG(ArrDelay)
 FROM flight a
-INNER JOIN airport b
+LEFT JOIN airport b
 ON b.iata=a.origin
 GROUP BY City, Year, Month
 ORDER BY City ASC;
 
-/* 5.Retard promig d’arribada dels vols, per mesos i segons l’aeroport origen (mateixa consulta que abans i amb el mateix ordre). 
-Però a més, ara volen que en comptes del codi de l’aeroport es mostri el nom de la ciutat. */
-USE usairlineflights;
-SELECT b.City, Year, Month, AVG(ArrDelay)
-FROM flight a
-INNER JOIN airport b
-ON b.iata=a.origin
-GROUP BY City, Year, Month
-ORDER BY City ASC;
-
-/* 6.Les companyies amb més vols cancelats. 
+/* 5.Les companyies amb més vols cancelats. 
 A més, han d’estar ordenades de forma que les companyies amb més cancelacions apareguin les primeres.*/
-USE usairlineflights;
 SELECT UniqueCarrier, COUNT(Cancelled)
 FROM flight a
 WHERE Cancelled=1
 GROUP BY UniqueCarrier
 ORDER BY COUNT(Cancelled) DESC;
 
-/* 7.L’identificador dels 10 avions que més distància han recorregut fent vols.*/
-USE usairlineflights;
+/* 6.L’identificador dels 10 avions que més distància han recorregut fent vols.*/
 SELECT TailNum, SUM(Distance)
 FROM flight
 WHERE TailNum!="NA"
@@ -52,11 +39,10 @@ GROUP BY TailNum
 ORDER BY SUM(Distance)
 DESC LIMIT 10;
 
-/* 8.Companyies amb el seu retard promig només d’aquelles les quals els seus vols arriben 
+/* 7.Companyies amb el seu retard promig només d’aquelles les quals els seus vols arriben 
 al seu destí amb un retràs promig major de 10 minuts.*/
-USE usairlineflights;
 SELECT UniqueCarrier, AVG(ArrDelay)
 FROM flight
-where ArrDelay > 10
-GROUP BY UniqueCarrier, ArrDelay
+GROUP BY UniqueCarrier
+HAVING AVG(ArrDelay) > 10
 ORDER BY AVG(ArrDelay);
